51b8d9c056e9724535a74e4516fcd3287745bb23
waynelogan <loganamollo@gmail.com>
2023-10-22 11:01:43 +0300

    added route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-        <Button variant={'default'} size={'lg'}>
-            Delete
-        </Button>
+      <Button variant={"default"} size={"lg"}>
+        Delete
+      </Button>
     </div>
-  )
+  );
 }
